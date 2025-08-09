930d0fa591862c5a034916e7708881793eb212b4
alejozd <alejozd79@gmail.com>
2025-08-09 13:14:27 -0500

    tema

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "primereact/resources/themes/lara-light-purple/theme.css"; // Un tema más suave
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
