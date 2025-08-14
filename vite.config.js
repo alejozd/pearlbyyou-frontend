9b49fe6b37efa7c408bfe58ce7e24b533aa5364a
alejozd <alejozd79@gmail.com>
2025-08-14 02:01:40 -0500

    mejora vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ export default defineConfig(({ mode }) => {
   // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd(), "");
 
+  // En desarrollo, el target del proxy es el backend local
+  const devTarget = new URL(env.VITE_API_URL).origin;
+
+  // En producción, el target del proxy es el base URL del servidor
+  // Asegúrate de que VITE_BASE_URL esté definido en .env.production
+  const prodTarget = env.VITE_BASE_URL;
+
+  const isProduction = mode === "production";
+
   return {
     plugins: [react()],
     server: {
@@ -12,7 +21,8 @@ export default defineConfig(({ mode }) => {
         "/api": {
           // target: "http://localhost:3003", // La dirección de tu backend
           // target: "https://pearlbyyou.sytes.net", // La dirección de tu backend
-          target: new URL(env.VITE_API_URL).origin,
+          // target: new URL(env.VITE_API_URL).origin,
+          target: isProduction ? prodTarget : devTarget,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, "/api"),
         },
