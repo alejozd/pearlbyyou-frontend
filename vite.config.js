586efb652fbbcd91dfe4dd190097655f04d5786d
alejozd <alejozd79@gmail.com>
2025-08-16 11:01:23 -0500

    warning

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd(), "");
 
   const proxyTarget = env.VITE_BASE_URL || "http://localhost:3003";
