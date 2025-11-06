dfc343369017f587f099c6b6589a533d122a2153
alejozd <alejozd79@gmail.com>
2025-11-06 16:50:13 -0500

    cmabios para admin

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const proxyTarget = env.VITE_BASE_URL || "http://localhost:3003";
 
   return {
+    base: "/",
     plugins: [react()],
     server: {
       proxy: {
